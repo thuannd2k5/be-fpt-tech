41ec1e9c37541cd12be15c723143f0fa213e77c7
thuannd2k5 <nguyenthuan22112005@gmail.com>
2026-08-20 08:01:58 +0700

    fix bug hiển thị thời gian hết hạn , tổng kết quá trình đã làm

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,9 +14,9 @@ import { JwtStrategy } from './passport/jwt.strategy';
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'),
+        secret: configService.get<string>('JWT_ACCESS_TOKEN'),
         signOptions: {
-          expiresIn: configService.get('JWT_EXPIRE'),
+          expiresIn: configService.get('JWT_ACCESS_EXPIRE'),
         },
       }),
       inject: [ConfigService],
